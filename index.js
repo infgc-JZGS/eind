c61af6530e1f6cb4046502bb8d19681b7b25cff3
JarcoScript <jarco.zetstra@gmail.com>
2021-01-19 11:34:10 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-require('colors');
 const Server = require('fastify')();
 const fs = require('fs');
+const ejs = require('ejs');
 
-Server.get('/', async (req, res) => {
-  file = fs.readFileSync('views/test.html');
-  res.type('text/html');
-  res.send(file);
+Server.get('/', async (request, reply) => {
+  page = fs.readFileSync('views/test.html', 'utf-8');
+  reply.type('text/html');
+  reply.send(page);
+});
+
+Server.get('/ip', async (request, reply) => {
+  file = fs.readFileSync('views/test.ejs', 'utf-8');
+  page = ejs.render(file, {
+    ip: request.ip,
+  });
+
+  reply.type('text/html');
+  reply.send(page);
 });
 
 Server.listen(3000, (err, addr) => {
   if (err) throw err;
-  console.log(`listening on ${addr}!`.brightGreen);
+  console.log(`listening on ${addr}!`);
 });
